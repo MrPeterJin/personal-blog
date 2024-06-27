e285ce28ae33084c856fbb8ab1e10802f227fafd
Peter Kam <51412849+MrPeterJin@users.noreply.github.com>
2024-06-27 23:10:58 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -34,7 +34,9 @@ const CONFIG = {
   HEO_INFOCARD_GREETINGS: [
     'Hi！I am',
   ],
-  HEO_INFO_CARD_URL: 'https://github.com/MrPeterJin', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL1: 'https://github.com/MrPeterJin', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL2: 'https://academic.peterkam.top', // 个人资料底部按钮链接
+  
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
