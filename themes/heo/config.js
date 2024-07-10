91313dc1fa32ef4f24d5626db8c7cddc499fb850
HKUSTSMARTLab <petergamsing@gmail.com>
2024-07-10 17:35:20 +0800

    Fix: icon

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -35,10 +35,10 @@ const CONFIG = {
     'Hi！I am',
   ],
 
-  HEO_INFO_CARD_URL1: 'https://github.com/MrPeterJin', // 个人资料底部按钮链接
-  HEO_INFO_CARD_ICON1: 'fab fa-github',
-  HEO_INFO_CARD_URL2: 'https://academic.peterkam.top', // 个人资料底部按钮链接
-  HEO_INFO_CARD_ICON2: 'fas fa-user',
+  HEO_INFO_CARD_URL1: 'https://academic.peterkam.top', // 个人资料底部按钮链接
+  HEO_INFO_CARD_ICON1: 'fas fa-user',
+  HEO_INFO_CARD_URL2: 'https://github.com/MrPeterJin', // 个人资料底部按钮链接
+  HEO_INFO_CARD_ICON2: 'fab fa-github',
   
   // 用户技能图标
   HEO_GROUP_ICONS: [
